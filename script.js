d411f48f17089892447f5c11c248894b07af9f99
CoreyAI <coreyiaquinta@gmail.com>
2022-06-18 21:21:06 -0400

    Updated for enhanced password validation check & feedback.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Declares and initializes form variable whenever a completed form is submitted.
 const form = document.querySelector("form");
 form.addEventListener("submit", function(e) {
-  e.preventDefault();
-  console.log("button pressed");
+  e.preventDefault(); // Prevents form submission from going through prior to validation.
   validation();
 });
 
+// Validates the form inputs and submits the form if all validations pass.
+// Note: Only password check is currently set up. Function is scalable for
+//       additional validation checks.
 function validation() {
   let valid = false;
   valid = passwordCheck();
@@ -14,15 +17,21 @@ function validation() {
   }
 }
 
+// Compares both password inputs to ensure that they match.
 function passwordCheck() {
   const pass1 = form.elements["password"].value;
   const pass2 = form.elements["password-confirm"].value;
   if (pass1 === pass2) {
     return true;
   } else {
-    form.elements["password"].style.border = "2px solid red";
-    form.elements["password-confirm"].style.border = "2px solid red";
+    passwordFalse();
     return false;
   }
 }
 
+// Provides user with error feedback for mis-matched passwords.
+function passwordFalse() {
+  form.elements["password"].style.border = "2px solid red";
+  form.elements["password-confirm"].style.border = "2px solid red";
+  form.querySelector("label[for=password] > .error").innerHTML = "* Passwords do not match";
+}
